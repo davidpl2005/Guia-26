68038fc514e28d4b6b8fb767678420c2ee5304a0
Isac pacheco <isac.pachecolopez@unicolombo.edu.co>
2024-04-24 19:07:26 -0500

    Vectores, Eventos

---
+++ guia_26/src/paquetePrincipal/Main.java
@@ -0,0 +1,17 @@
+
+package paquetePrincipal;
+
+/**
+ *
+ * @author isac pacheco
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
